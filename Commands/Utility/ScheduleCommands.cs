50c822e8ea8cf133cf1b4d0c0b2aa5f72066615b
RendyMod <rendytgnp@laposte.net>
2023-12-01 05:11:21 +0100

    Fix parameter in globalcommands schedule

--- Commands/Utility/ScheduleCommands.cs
+++ Commands/Utility/ScheduleCommands.cs
@@ -7,7 +7,7 @@ using PvpArena.Models;
 public static class ScheduleCommands
 {
 	[CommandFramework.Command("globaldiscord", description: "Give the link of the discord to everyone", usage:".globaldiscord", adminOnly: true, includeInHelp: false, category: "Misc")]
-	public static void GlobalDiscordCommand()
+	public static void GlobalDiscordCommand(Player sender)
 	{
 		string message = ("Join us".Emphasize() + " on Discord: ").White() +
 		                 $"{PvpArenaConfig.Config.DiscordLink}".Colorify(ExtendedColor.LightServerColor).Emphasize();
@@ -18,20 +18,12 @@ public static class ScheduleCommands
 	}
 	
 	[CommandFramework.Command("globalrankedhelp", description: "Send a message to everyone for ranked", usage:".rankedhelp", adminOnly: true, includeInHelp: false, category: "Misc")]
-	public static void GlobalRankedHelpCommand()
+	public static void GlobalRankedHelpCommand(Player sender)
 	{
 		string message =  ("Rankeds".Emphasize()+ " are out!" + " Join queue".Emphasize() + " with: ").White() + ".ranked join".Colorify(ExtendedColor.LightServerColor).Emphasize();
 		ServerChatUtils.SendSystemMessageToAllClients(
 			VWorld.Server.EntityManager,
 			message
 		);
-	}
-	
-	[CommandFramework.Command("testrendy", description: "Send a message to everyone for ranked", usage:".rankedhelp", adminOnly: true, includeInHelp: false, category: "Misc")]
-	public static void GlobalRankedHelpCommand(Player sender)
-	{
-		string message = ("Join us".Emphasize() + " on Discord: ").White() +
-		                 $"{PvpArenaConfig.Config.DiscordLink}".Colorify(ExtendedColor.LightServerColor).Emphasize();
-		sender.ReceiveMessage(message);
 	}
 }
