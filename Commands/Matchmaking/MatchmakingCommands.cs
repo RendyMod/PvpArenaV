b1d8c5eb2d6ef47343619e93962b1ac4fe6e209d
RendyMod <rendytgnp@laposte.net>
2024-01-07 00:20:40 +0100

    Ranked commands enabled

--- Commands/Matchmaking/MatchmakingCommands.cs
+++ Commands/Matchmaking/MatchmakingCommands.cs
@@ -12,7 +12,7 @@ namespace PvpArena.Commands;
 
 internal static class MatchmakingCommands
 {
-	//[Command("ranked join", description: "Enters the matchmaking queue", aliases: new string[] { "join" }, adminOnly: false, includeInHelp: false, category: "Ranked")]
+	[Command("ranked join", description: "Enters the matchmaking queue", aliases: new string[] { "join" }, adminOnly: false, includeInHelp: false, category: "Ranked")]
 	public static void JoinMatchmakingCommand(Player sender, bool autoRequeue = false)
 	{
 		if (!PvpArenaConfig.Config.MatchmakingEnabled)
@@ -26,7 +26,7 @@ internal static class MatchmakingCommands
 		sender.ReceiveMessage($"Joined the queue. Auto Re-queue: {autoRequeue.ToString().Emphasize()}".White());
 	}
 
-	//[Command("ranked leave", description: "Leaves the matchmaking queue", aliases: new string[] { "leave" }, adminOnly: false, includeInHelp: false, category: "Ranked")]
+	[Command("ranked leave", description: "Leaves the matchmaking queue", aliases: new string[] { "leave" }, adminOnly: false, includeInHelp: false, category: "Ranked")]
 	public static void LeaveMatchmakingCommand(Player sender)
 	{
 		if (!PvpArenaConfig.Config.MatchmakingEnabled)
@@ -40,7 +40,7 @@ internal static class MatchmakingCommands
 		sender.ReceiveMessage("Left the queue!".Success());
 	}
 
-	/*[Command("lb ranked", description: "Displays the ranked leaderboard", adminOnly: false, includeInHelp: false, category: "Ranked")]*/
+	[Command("lb ranked", description: "Displays the ranked leaderboard", adminOnly: false, includeInHelp: false, category: "Ranked")]
 	public static void ShowLeaderboardCommand(Player sender, int pageNumber = 1)
 	{
 		const int playersPerPage = 10; // Number of players to display per page
